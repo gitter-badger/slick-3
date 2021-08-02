2c82abf5c7c8bde6e7ba490d00322ec121847fef
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-08-02 11:10:58 +0200

    adding lexer module

--- cpp/lexer.cpp
+++ cpp/lexer.cpp
@@ -1,1 +1,12 @@
+#include "lexer.hpp"
+#include "utils.hpp"
 
+#include <iostream>
+#include <sstream>
+
+namespace Slick
+{
+  std::map<char TokenType> singleTokens {}
+  std::map<char TokenTyüe> operatorTokens {}
+  std::map<char TokenType> keywordTokens {}
+}
